a7a335cc1417a0f28238df0c0c5698344ddc2e8c
IAmProgrammist <vladislav.pakhomov@bk.ru>
2023-10-10 21:29:03 +0300

    Magic numbers are not magic anymore

--- lab4/main.c
+++ lab4/main.c
@@ -2,12 +2,16 @@
 
 #include <assert.h>
 
+#define LOW 50
+#define HIGH 450
+#define STEP 50
+
 int main() {
-    comparesSearchExperiment       (linearSearch,             "linear search",                          50, 450, 50);
-    comparesSearchExperiment       (linearQuickSearch,        "quick linear search",                    50, 450, 50);
-    comparesOrderedSearchExperiment(orderedLinearQuickSearch, "quick linear search for ordered arrays", 50, 450, 50);
-    comparesOrderedSearchExperiment(orderedBinarySearch,      "binary search",                          50, 450, 50);
-    comparesOrderedSearchExperiment(orderedBlockSearch,       "block search",                           50, 450, 50);
+    comparesSearchExperiment       (linearSearch,             "linear search",                          LOW, HIGH, STEP);
+    comparesSearchExperiment       (linearQuickSearch,        "quick linear search",                    LOW, HIGH, STEP);
+    comparesOrderedSearchExperiment(orderedLinearQuickSearch, "quick linear search for ordered arrays", LOW, HIGH, STEP);
+    comparesOrderedSearchExperiment(orderedBinarySearch,      "binary search",                          LOW, HIGH, STEP);
+    comparesOrderedSearchExperiment(orderedBlockSearch,       "block search",                           LOW, HIGH, STEP);
 
     return 0;
 }
